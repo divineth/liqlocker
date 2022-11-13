de062c8890b17b6eeaeabee24091e841e03c8c15
divineth <divinethomson@outlook.com>
2022-11-13 15:39:31 +0000

    Update site logos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,10 +33,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en" dir="ltr">
         <Head>         
-          <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
-          <link rel="shortcut icon" href="/icon.png" />
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
